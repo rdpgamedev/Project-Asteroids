66eb5587e5aa9a8d24178bdc0740f3dd5d08fbd3
RichardP <rdpgamedev@gmail.com>
2016-08-21 07:06:15 -0700

    Test Scene and Scripts
    Created a Test Scene for testing new features in.
    Created a SpawnTestAsteroids script that spawns N asteroids in a random
    volume with random scales.
    Created a Rotation script that rotates the object in the 3 cardinal axes
    with random speeds.

---
+++ Assets/Rotation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+    //Tweakable variables from editor.
+    public float MAXIMUM_SPEED = 2f;
+
+    //Remaining variables for the script.
+    float deltaAngleX, deltaAngleY, deltaAngleZ;
+    Vector3 eulerRotation;
+
+
+	// Use this for initialization
+	void Start () {
+        deltaAngleX = Random.Range(-MAXIMUM_SPEED, MAXIMUM_SPEED);
+        deltaAngleY = Random.Range(-MAXIMUM_SPEED, MAXIMUM_SPEED);
+        deltaAngleZ = Random.Range(-MAXIMUM_SPEED, MAXIMUM_SPEED);
+        eulerRotation = new Vector3(deltaAngleX, deltaAngleY, deltaAngleZ);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(eulerRotation);
+	}
+}
+
