5a7984849a1e05c10d84966980f1b6efc167f6bc
RichardP <rdpgamedev@gmail.com>
2016-11-29 17:50:55 -0800

    Checkpoint Collision and misc
    Started Checkpoint collision on its membrane
    Checkpoints now start field segments
    Membranes have a box collider for trigger
    Checkpoints are parented to their field segment

---
+++ Assets/Scripts/MembraneCollider.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MembraneCollider : MonoBehaviour {
+    public GameObject CHECKPOINT;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+        }
+    }
+}
