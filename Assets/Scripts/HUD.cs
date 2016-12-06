d1cba63ab59d1408985b84d47dc489955ed20ca8
RichardP <rdpgamedev@gmail.com>
2016-12-06 03:43:36 -0800

    Score Formatting
    Score Formatting for 3 digits followed by suffix

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -29,8 +29,43 @@ public class HUD : MonoBehaviour {
 
     void UpdateScore ()
     {
+        int score = GameManager.instance.score;
+        int thousands = 0;
+        while (score > 999)
+        {
+            score /= 1000;
+            ++thousands;
+        }
+        string padding = "";
+        if (score > 9) padding += " ";
+        if (score > 99) padding += " ";
+        string suffix = "";
+        switch (thousands)
+        {
+            case 0:
+                suffix = "";
+                break;
+            case 1:
+                suffix = "K";
+                break;
+            case 2:
+                suffix = "M";
+                break;
+            case 3:
+                suffix = "B";
+                break;
+            case 4:
+                suffix = "T";
+                break;
+            case 5:
+                suffix = "Q";
+                break;
+            default:
+                suffix = "?!?";
+                break;
+        }
         SCORETEXT.GetComponent<TextMesh>().text =
-            "SCORE: " + GameManager.instance.score;
+            "SCORE: " + padding + score + suffix;
     }
 
     void UpdateTime ()
