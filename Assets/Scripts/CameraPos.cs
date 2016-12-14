686ab5de9b9f489d823dcce8f893890803e002e2
RichardP <rdpgamedev@gmail.com>
2016-12-14 14:38:44 -0800

    Camera Boost at Checkpoint
    Crossing a checkpoint pushes the camera back
    to simultae a boost in speed.

--- Assets/Scripts/CameraPos.cs
+++ Assets/Scripts/CameraPos.cs
@@ -2,11 +2,21 @@
 using System.Collections;
 
 public class CameraPos : MonoBehaviour {
-    public float SCALE = 1f;
+    public static CameraPos instance;
 
+    public float SCALE = 1f;
+    public float ZOOMSCALE = 40f;
+    public float ZOOMRATIO = 8f;
+
+    private float zoomOffset;
     private Rigidbody SHIPBODY;
     private Transform SHIPTRANS;
     private Vector3 STARTPOS;
+
+    void Awake ()
+    {
+        instance = this;
+    }
 
 	void Start () {
         SHIPBODY = PlayerShip.instance.GetComponent<Rigidbody>();
@@ -18,6 +28,13 @@ public class CameraPos : MonoBehaviour {
         Vector3 offset = SHIPTRANS.forward.normalized - SHIPBODY.velocity.normalized;
         offset *= SCALE;
         Vector3 delta = offset - transform.localPosition;
+        offset.z -= zoomOffset;
+        zoomOffset /= ZOOMRATIO;
         transform.localPosition = offset;
 	}
+
+    public void CheckpointZoom ()
+    {
+        zoomOffset = ZOOMSCALE;
+    }
 }
