8fadc711a33c1b717556ea37b53c2f04a59e120c
RichardP <rdpgamedev@gmail.com>
2016-12-14 13:22:46 -0800

    First Pass
    Fixed Jittering by using FixedUpdate
    Renamed Velocity Indicator to Camera Target
    Used Transform.Translate() instead of updating
    raw positions.

---
+++ Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTarget : MonoBehaviour
+{
+    PlayerShip ship;
+    Rigidbody shipbody;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        ship = PlayerShip.instance;
+        shipbody = ship.gameObject.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 targetPos = ship.transform.position + shipbody.velocity * 0.5f + ship.transform.forward * 20f;
+        Vector3 delta = targetPos - transform.position;
+        transform.Translate(delta, Space.World);
+    }
+}
