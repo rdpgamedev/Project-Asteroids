72d42eca9d9e4409feb9bd47ce391e6e591c9c8e
RichardP <rdpgamedev@gmail.com>
2017-01-04 21:59:10 -0800

    Highscores class
    Class to keep scores in a list
    Includes xml load and write functions

---
+++ Assets/Scripts/Highscores.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;
+
+[XmlRoot("Highscores")]
+public class Highscores{
+    [XmlArray("Scores")]
+    [XmlArrayItem("Score")]
+    public List<Score> Scores = new List<Score>();
+
+    public void Save(string path)
+    {
+        var serializer = new XmlSerializer(typeof(Highscores));
+        using (var stream = new FileStream(path, FileMode.Create))
+        {
+            serializer.Serialize(stream, this);
+        }
+    }
+
+    public static Highscores Load(string path)
+    {
+        var serializer = new XmlSerializer(typeof(Highscores));
+        using (var stream = new FileStream(path, FileMode.Open))
+        {
+            return serializer.Deserialize(stream) as Highscores;
+        }
+    }
+
+    //Loads the xml directly from the given string. Useful in combination with www.text.
+    public static Highscores LoadFromText(string text)
+    {
+        var serializer = new XmlSerializer(typeof(Highscores));
+        return serializer.Deserialize(new StringReader(text)) as Highscores;
+    }
+
+}
