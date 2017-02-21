88f85dd9de1388e5c0578236e076233031164e35
RichardP <rdpgamedev@gmail.com>
2017-02-20 18:58:31 -0800

    Added Boost
    Added a further increase to speed when boosting
    Need to fix twitchy turning and add more effects

--- Assets/Scripts/ThrusterController.cs
+++ Assets/Scripts/ThrusterController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class ThrusterController : MonoBehaviour
 {
     public static float MAXTHRUST;
+    public float BOOST_THRESHOLD = 0.9f;
     public bool isLeft = false;
     public bool active = false;
 
@@ -31,17 +32,20 @@ public class ThrusterController : MonoBehaviour
         Rigidbody shipbody = ship.GetComponent<Rigidbody>();
         Vector3 direction = transform.forward;
         Vector3 force = direction * thrust * thrustScale;
+        if (thrust > BOOST_THRESHOLD) force *= 1.2f;
         if (!GameManager.instance.isPlaying) force *= 0f;
         //use point between thruster and ship as application point
         Vector3 thrusterpos = this.transform.position;
-        Vector3 offset = ship.transform.position - thrusterpos;
+        Vector3 offset = ship.GetComponent<Rigidbody>().worldCenterOfMass - thrusterpos;
         float offsetMin = PlayerShip.instance.THRUSTER_OFFSET_MIN;
         if (offsetMin > 0.5f) offsetMin = 0.5f;
         if (offsetMin < 0f) offsetMin = 0f;
-        offset *= thrust / 2 + offsetMin;
+        offset *= thrust / 4f + offsetMin;
         Vector3 forcepos = thrusterpos + offset;
         shipbody.AddForceAtPosition(force, forcepos);
-
+        Debug.DrawLine(forcepos, forcepos+force);
+        Debug.DrawLine(transform.position, transform.position+direction*5f, Color.green);
+        Debug.DrawLine(ship.GetComponent<Rigidbody>().worldCenterOfMass, ship.GetComponent<Rigidbody>().worldCenterOfMass + direction * 5f, Color.red);
         ParticleSystem thrusterParticles = 
             transform.FindChild("Thruster_Particles").GetComponent<ParticleSystem>();
         ParticleSystem.EmissionModule emission = thrusterParticles.emission;
@@ -78,6 +82,7 @@ public class ThrusterController : MonoBehaviour
     // (e.g. when the left stick is pushed to the right)
     float ThrustCalculation (float yaxis, float xaxis)
     {
-        return (yaxis + 1f) / 6f + (2f / 3f) - Mathf.Abs(xaxis) / 8f;
+        //thrust ranges from 2/3 to 1
+        return (yaxis + 1f) / 6f + (2f / 3f);
     }
 }
