835bd9daaf9b5c1ca068fb5b2e78942aeea952ac
RichardP <rdpgamedev@gmail.com>
2017-03-10 21:12:06 -0800

    Url Opening
    Added Link.cs
    Holds a url and function to open url
    OpenURL() called by event system on pointer click

---
+++ Assets/Scripts/Link.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : MonoBehaviour
+{
+    public string url;
+
+    public void OpenURL()
+    {
+        Application.OpenURL(url);
+    }
+}
