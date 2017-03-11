7ec650a1c2fe289b36e4be14935d2691cf006449
RichardP <rdpgamedev@gmail.com>
2017-03-10 22:08:34 -0800

    Link functionality and Credits restructure
    Added highlighting to Link text
    Restructured Credits to utilize Links
    Simplified Credits

--- Assets/Scripts/Link.cs
+++ Assets/Scripts/Link.cs
@@ -1,13 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Link : MonoBehaviour
 {
     public string url;
+    public Color normalColor;
+    public Color highlightedColor;
 
-    public void OpenURL()
+    private Text text;
+
+    public void HighlightColor()
+    {
+        text.color = highlightedColor;
+    }
+
+    public void NormalColor()
+    {
+        text.color = normalColor;
+    }
+
+    public void OpenUrl()
     {
         Application.OpenURL(url);
+    }
+
+    private void Start()
+    {
+        text = gameObject.GetComponent<Text>();
+    }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("mouse entered");
     }
 }
